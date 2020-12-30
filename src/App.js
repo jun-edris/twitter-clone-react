2f3961602d60c352a866b90bb187b4df2c8a0e54
Junaire Edris B. Buico <juned.buico@gmail.com>
2020-12-30 20:10:59 +0800

    creating the login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Login from './Routes/Login';
+
 
 function App() {
   return (
     <div className="app">
-      <h1>HI</h1>
-      <h2>Junaire</h2>
+      <Router>
+        <Switch>
+          <Route>
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
